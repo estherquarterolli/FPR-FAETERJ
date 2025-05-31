023e5d80ebb8a6256df86c454cdd40a0d171462d
Esther Quarterolli <115496105+estherquarterolli@users.noreply.github.com>
2025-05-31 14:13:06 -0300

    att

--- ORDENAÇÃO/INSERTION.C
+++ ORDENAÇÃO/INSERTION.C
@@ -20,7 +20,7 @@ int main() {
     int arr[] = {5, 1, 4, 2, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
     insertionSortSimples(arr, n);
-    printf("Array ordenado (Insertion Sort Simples): ");
+    printf("Array ordenado (Insertion Sort   Simples): ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
